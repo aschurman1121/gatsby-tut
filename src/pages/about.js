6feb62cceafe9c7ff42a80386f7bb71067da5e41
Alexandra Schurman <alexandraschurman@gmail.com>
2023-01-06 10:40:56 -0500

    second

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h2>about me!</h2>
+            <p> hulo I am creatin this site</p>
+        </main>
+    )
+}
+
+export const Head = () =>(
+        <>
+            <title>about Ali</title>
+            <meta name='description' content='desc'/>
+        </>
+)
+
+
+export default AboutPage
