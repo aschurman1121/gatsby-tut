f530f08476dd3c297a61248587bfcb5713aeeae1
Alexandra Schurman <alexandraschurman@gmail.com>
2023-01-09 08:15:22 -0800

    monday

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p> this is following the tutu I found</p>
+      <StaticImage 
+      alt='cocker spaniel with flower'
+      src='https://as1.ftcdn.net/v2/jpg/02/75/98/22/1000_F_275982235_xzqfvupzoy03oAiWf7iFTpTcpvVaR4TF.jpg'
+      />
     </Layout>
   )
 }
