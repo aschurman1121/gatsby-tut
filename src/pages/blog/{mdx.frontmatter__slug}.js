94d83f25ff924ee6e346c3ef4ddd569ceeab2ce5
Alexandra Schurman <alexandraschurman@gmail.com>
2023-01-15 15:10:00 -0800

    not quite there yet, still have some digging

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,27 +1,45 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/Seo'
 
 
 const BlogPost = ( {data, children}) => {
+
+
+    const image = getImage(data.mdx.frontmatter.hero_image)
+    
     return (
         <Layout pageTitle={data.mdx.frontmatter.title} > 
-            <p> {data.mdx.frontmatter.date} </p>
+            <p> posted: {data.mdx.frontmatter.date} </p>
+            <GatsbyImage 
+                imgage={image}
+                alt= {data.mdx.frontmatter.hero_image_alt} />
             {children}
         </Layout>
     )
 } 
 
 
-export const query = graphql`query MyQuery($id: String) {
+export const query = graphql`
+query($id: String) {
     mdx(id: {eq: $id}) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
-  } `
+  }
+`
 
 
 export const Head = ({data}) => <Seo title={data.mdx.frontmatter.title}/>
