03f6703d2baca70ba22ca775015929b3ce45cbee
Alexandra Schurman <alexandraschurman@gmail.com>
2023-01-20 11:58:58 -0800

    playing around

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -3,10 +3,28 @@ import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/Seo'
+import { useState, useEffect } from "react"
 
 
 const BlogPost = ( {data, children}) => {
 
+  const URL = 'https://place.dog'
+
+  const [dog, setDog] = useState()
+
+
+  useEffect(() => {
+      const fetchData = async () => {
+        const result = await fetch(URL)
+        result.json().then( json => {
+          console.log(json);
+          setDog(json.whatever.whatever)
+        })
+        // console.log(result)
+
+      }
+      fetchData(); 
+  }, [])
 
     const image = getImage(data.mdx.frontmatter.hero_image)
     
@@ -17,12 +35,16 @@ const BlogPost = ( {data, children}) => {
                 image={image}
                 alt= {data.mdx.frontmatter.hero_image_alt} />
             <p>
+              <div> 
+                Here is a dog: {dog} 
+
+              </div>
               Photo Credit:{ " " }
               <a href={data.mdx.frontmatter.hero_image_credit_link}>
                 {data.mdx.frontmatter.hero_image_credit_text}
               </a>
             </p>
-            {children}
+            {/* {children} */}
         </Layout>
     )
 } 
